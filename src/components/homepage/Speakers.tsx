e27e5420b1e7e20a170b7bbf414e1330509203f1
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2025-02-15 09:55:05 +0700

    feat: speaker more responsive

--- src/components/homepage/Speakers.tsx
+++ src/components/homepage/Speakers.tsx
@@ -7,20 +7,20 @@ export default function Speakers() {
             <h2 className="headerText text-center text-linen [text-shadow:0px_0px_10px_#6E8EEC;]">
                 PREVIOUS SPEAKERS
             </h2>
-            <div className="flex gap-12 mt-8 flex-wrap justify-evenly">
+            <div className="flex gap-2 md:gap-12 mt-8 flex-wrap justify-evenly">
                 <div className="flex flex-col items-center">
                     <div className="relative mt-5">
                         <Image
                             src={profile}
                             alt="profile"
-                            className="z-10 relative"
+                            className="z-10 relative max-md:w-[120px]"
                         />
                         <div
-                            className="absolute bg-cornflowerBlue w-[150px] h-[200px] bottom-0 z-0 left-4
-                            [background:linear-gradient(121deg,_#D0D6E9_0%,#6E8EEC_100%)]"
+                            className="absolute bg-cornflowerBlue w-[90px] md:w-[150px] aspect-[3/4] bottom-0 z-0 left-4
+                            [background:linear-gradient(121deg,_#D0D6E9_0%,#6E8EEC_100%)] aspect"
                         ></div>
                     </div>
-                    <div className="flex flex-col items-center gap-1 mt-4 font-medium">
+                    <div className="flex flex-col items-center gap-1 mt-4 font-medium text-center">
                         <p>Lorem ipsum</p>
                         <p>Lorem ipsum dolor sit amet.</p>
                     </div>
@@ -30,14 +30,14 @@ export default function Speakers() {
                         <Image
                             src={profile}
                             alt="profile"
-                            className="z-10 relative"
+                            className="z-10 relative max-md:w-[120px]"
                         />
                         <div
-                            className="absolute bg-cornflowerBlue w-[150px] h-[200px] bottom-0 z-0 left-4
-                            [background:linear-gradient(121deg,_#D0D6E9_0%,#6E8EEC_100%)]"
+                            className="absolute bg-cornflowerBlue w-[90px] md:w-[150px] aspect-[3/4] bottom-0 z-0 left-4
+                            [background:linear-gradient(121deg,_#D0D6E9_0%,#6E8EEC_100%)] aspect"
                         ></div>
                     </div>
-                    <div className="flex flex-col items-center gap-1 mt-4 font-medium">
+                    <div className="flex flex-col items-center gap-1 mt-4 font-medium text-center">
                         <p>Lorem ipsum</p>
                         <p>Lorem ipsum dolor sit amet.</p>
                     </div>
@@ -47,18 +47,19 @@ export default function Speakers() {
                         <Image
                             src={profile}
                             alt="profile"
-                            className="z-10 relative"
+                            className="z-10 relative max-md:w-[120px]"
                         />
                         <div
-                            className="absolute bg-cornflowerBlue w-[150px] h-[200px] bottom-0 z-0 left-4
-                            [background:linear-gradient(121deg,_#D0D6E9_0%,#6E8EEC_100%)]"
+                            className="absolute bg-cornflowerBlue w-[90px] md:w-[150px] aspect-[3/4] bottom-0 z-0 left-4
+                            [background:linear-gradient(121deg,_#D0D6E9_0%,#6E8EEC_100%)] aspect"
                         ></div>
                     </div>
-                    <div className="flex flex-col items-center gap-1 mt-4 font-medium">
+                    <div className="flex flex-col items-center gap-1 mt-4 font-medium text-center">
                         <p>Lorem ipsum</p>
                         <p>Lorem ipsum dolor sit amet.</p>
                     </div>
                 </div>
+            
             </div>
         </div>
     );
